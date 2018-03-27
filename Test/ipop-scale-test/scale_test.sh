e5638046587421f42cba5300a8c764bc659d5f42
Ashish Nargundkar <apnargundkar@gmail.com>
2018-03-27 12:37:30 -0400

    scale_test.sh: Numeric choice for mode
    User has to input 1 for classic mode and 2 for switch mode instead of typing out the whole thing themselves.

--- Test/ipop-scale-test/scale_test.sh
+++ Test/ipop-scale-test/scale_test.sh
@@ -589,12 +589,21 @@ function logs
 function check-vpn-mode
 {
     if [ -z $VPNMODE ] ; then
-        echo -e "Select vpn mode to test: classic or switch"
-        read VPNMODE
+        echo -e "Select vpn mode to test. Please input 1 for classic or 2 for switch."
+        read VPNMODE_CODE
+        while [ -z $VPNMODE_CODE ] || ([ "$VPNMODE_CODE" != "1" ] && [ "$VPNMODE_CODE" != "2" ]) ; do
+            echo -e "Incorrect input. Please input 1 for classic or 2 for switch."
+            read VPNMODE_CODE
+        done
+
+        if [ "$VPNMODE_CODE" == "1" ] ; then
+            VPNMODE="classic"
+        else
+            VPNMODE="switch"
+        fi
         echo "MODE $VPNMODE" >> $OVERRIDES_FILE
     fi
 }
-
 function configure-external-node
 {
     username=$1
