d554942af6986937ff8fd861efba7cc43eb18d62
Ashish Nargundkar <apnargundkar@gmail.com>
2018-03-30 18:25:37 -0400

    scale_test.sh: breaks VALID_COMMANDS into parts

--- Test/ipop-scale-test/scale_test.sh
+++ Test/ipop-scale-test/scale_test.sh
@@ -654,7 +654,12 @@ $@
 
 # NOTE MAKE SURE that you add any new commands here,
 # otherwise the user won't be able to use them!
-VALID_COMMANDS="install-support-serv prep-def-container containers-create containers-start containers-del containers-stop containers-update ipop-start ipop-stop ipop-status quit visualizer-start visualizer-stop visualizer-status ipop-tests logs mode help"
+VALID_COMMANDS="install-support-serv prep-def-container containers-create"
+VALID_COMMANDS="$VALID_COMMANDS containers-start containers-del"
+VALID_COMMANDS="$VALID_COMMANDS containers-stop containers-update ipop-start"
+VALID_COMMANDS="$VALID_COMMANDS ipop-stop ipop-status quit visualizer-start"
+VALID_COMMANDS="$VALID_COMMANDS visualizer-stop visualizer-status ipop-tests"
+VALID_COMMANDS="$VALID_COMMANDS logs mode help"
 
 show_help=true
 while [ -z $user_input ] ; do
