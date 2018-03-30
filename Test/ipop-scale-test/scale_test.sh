59c0e0749c465a49b58fe69261da59d434c98886
Ashish Nargundkar <apnargundkar@gmail.com>
2018-03-30 19:44:04 -0400

    Fixes a bug using a non-existent file

--- Test/ipop-scale-test/scale_test.sh
+++ Test/ipop-scale-test/scale_test.sh
@@ -626,20 +626,20 @@ function ipop-tests
 function mode
 {
     action=$1
-    current_vpn_mode=$(cat $HELP_FILE 2>/dev/null | grep MODE | awk '{print $2}')
+    current_vpn_mode=$(cat $OVERRIDES_FILE 2>/dev/null | grep MODE | awk '{print $2}')
     case $action in
         "change")
             if [[ "$current_vpn_mode" == "classic" ]]; then
+                sed -i "s/MODE .*/MODE switch/g" $OVERRIDES_FILE
                 echo "Mode changed to switch."
-                sed -i "s/MODE .*/MODE switch/g" $HELP_FILE
             else
+                sed -i "s/MODE .*/MODE classic/g" $OVERRIDES_FILE
                 echo "Mode changed to classic."
-                sed -i "s/MODE .*/MODE classic/g" $HELP_FILE
             fi
-            ;;
+        ;;
         *)
             echo "Current mode: $current_vpn_mode"
-            ;;
+        ;;
     esac
 }
 
